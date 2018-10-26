01fe11766a165497dce32150791a95614325434f
Bruno <bruno.silva@catho.com>
2018-10-26 19:26:04 -0300

    validação das informações

--- cadastrar.php
+++ cadastrar.php
@@ -1,6 +1,29 @@
 <?php
 
-$dados = $_POST['content'];
-echo '<li>'.$dados.'</li>';
+//$dados = $_POST['ra'];
+
+$valido = true;
+
+if (!$valido) {
+    http_response_code(404);
+    die;
+}
+
+if ($valido) {
+    $response = [
+        'id'  => 1,
+        'nome' => 'Barreto',
+        'holaaaa' => [
+            'a',
+            'b'
+        ],
+        'email' => 'barreto_monstro@catho.com'
+    ];
+    echo json_encode($response);
+}
+
+http_response_code(200);
+die;
+
 
 ?>

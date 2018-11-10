ab3fed3fc4f05a2ebbd5ac538050af93a4f2dc10
Bruno Miranda <bruno.silva@catho.com>
2018-11-10 16:36:42 -0200

    ajustando query, refatoração

--- model/selecionarDados.php
+++ model/selecionarDados.php
@@ -4,8 +4,8 @@ $conexao = mysqli_connect("localhost", "bruno", "", "evasao");
 function carregarDados($conexao)
 {
     $RAs = array();
-    $query = 'SELECT A.id, A.RA, status, data_entrada AS data FROM alunos AS A INNER JOIN matricula AS M ON A.matricula_id = M.id
-                INNER JOIN frequencia AS F ON A.id = F.alunos_id ORDER BY data DESC';
+    $query = 'SELECT A.id, A.RA, status, YEAR(data_entrada) AS ano, MONTH(data_entrada) AS mes, DAY(data_entrada) AS dia, TIME(data_entrada) AS horario FROM alunos AS A
+                INNER JOIN matricula AS M ON A.matricula_id = M.id LEFT JOIN frequencia AS F ON A.id = F.alunos_id ORDER BY dia DESC, horario DESC';
     $resultado = mysqli_query($conexao, $query);
     while ($RA = mysqli_fetch_assoc($resultado))
     {
@@ -17,8 +17,9 @@ function carregarDados($conexao)
 function carregarData($conexao, $id)
 {
     $datas = array();
-    $query = "SELECT data_entrada AS saida FROM alunos AS A LEFT JOIN frequencia AS F ON A.RA = F.RA
-                INNER JOIN matricula AS M ON A.matricula_id = M.id WHERE A.id = '{$id}' ORDER BY saida ASC";
+    $query = "SELECT nome, YEAR(data_entrada) AS anoEntrada, MONTH(data_entrada) AS mesEntrada, DAY(data_entrada) AS diaEntrada, TIME(data_entrada) AS horarioEntrada FROM alunos AS A
+                LEFT JOIN frequencia AS F ON A.RA = F.RA INNER JOIN matricula AS M ON A.matricula_id = M.id
+                    WHERE A.id = '{$id}' ORDER BY diaEntrada ASC, horarioEntrada ASC";
     $resultado = mysqli_query($conexao, $query);
     while ($data = mysqli_fetch_assoc($resultado))
     {

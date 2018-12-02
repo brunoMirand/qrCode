7adb27f50ed20b9420c35410e854c8840fda4dea
Bruno Miranda <bruno.silva@catho.com>
2018-12-02 11:13:51 -0200

    ajustes na query

--- model/selecionarDados.php
+++ model/selecionarDados.php
@@ -4,7 +4,7 @@ $conexao = mysqli_connect("localhost", "bruno", "", "evasao");
 function carregarDados($conexao, $id)
 {
     $query = "SELECT A.id, A.RA, A.foto, status, YEAR(data_entrada) AS ano, MONTH(data_entrada) AS mes, DAY(data_entrada) AS dia, TIME(data_entrada) AS horario FROM alunos AS A
-                INNER JOIN matricula AS M ON A.matricula_id = M.id LEFT JOIN frequencia AS F ON A.id = F.alunos_id WHERE A.id = '{$id}' ORDER BY dia DESC, horario DESC";
+                INNER JOIN matricula AS M ON A.matricula_id = M.id LEFT JOIN frequencia AS F ON A.id = F.alunos_id WHERE A.id = '{$id}' ORDER BY mes DESC, dia DESC, horario DESC";
     $resultado = mysqli_query($conexao, $query);
     $dados = mysqli_fetch_assoc($resultado);
     return $dados;
